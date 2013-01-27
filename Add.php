3279509ed497117be700fe45e7893c3a7d55b8f3
etimzaaa <dogtemples@gmail.com>
2013-01-27 21:49:26 +0700

    dd Signed-off-by: etimzaaa <dogtemples@gmail.com>

---
+++ Add.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:: Furniture Desing ::</title>
+<style type="text/css">
+<!--
+body {
+	margin-top: 0px;
+	margin-left: 0px;
+}
+#fb-root2 table tr th {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 11px;
+	color: #FFF;
+}
+#fb-root2 table tr th {
+	font-size: 12px;
+}
+#fb-root2 #edit table {
+	text-align: left;
+}
+#fb-root2 #edit table tr th table tr th {
+	color: #000;
+	text-align: left;
+	font-size: 12px;
+	font-weight: bold;
+}
+#fb-root2 #edit table tr th table {
+	text-align: left;
+	font-size: large;
+	font-weight: bold;
+}
+#fb-root2 #edit table tr th table tr th {
+	font-size: 12px;
+}
+#fb-root2 #edit table tr th table tr th00 {
+	font-family: Arial, Helvetica, sans-serif;
+}
+#fb-root2 #edit table tr th table tr th {
+	font-size: medium;
+	font-family: "ms Sans Serif";
+}
+#add {
+	color: #F00;
+}
+-->
+</style></head>
+
+<body>
+<div id="fb-root2" >
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <th align="right" valign="middle" bgcolor="#333333" scope="row">::&nbsp;&nbsp;</th>
+      <th align="left" valign="middle" bgcolor="#333333" scope="row">หน้าแรก&nbsp;&nbsp;&nbsp;    วิธีสั่งซื้อ     &nbsp;&nbsp;การชำระเงิน&nbsp;&nbsp;     การจัดส่งสินค้า&nbsp;&nbsp;     เว็บบอร์ด     &nbsp;&nbsp;ติดต่อเรา  &nbsp;&nbsp;::</th>
+      <th align="right" valign="middle" bgcolor="#333333" scope="row"><table width="186" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <th align="left" valign="top" bgcolor="#333333" scope="row" height="31px"><img src="product/login.gif" alt="" width="23" height="31" align="absmiddle" /></th>
+          <th align="left" valign="middle" bgcolor="#333333" scope="row">เข้าสู่ระบบ</th>
+          <th align="left" valign="top" bgcolor="#333333" scope="row"><img src="product/member.gif" alt="" width="22" height="31" align="absmiddle" /></th>
+          <th align="left" valign="middle" bgcolor="#333333" scope="row">สมัครสมาชิก</th>
+        </tr>
+      </table></th>
+    </tr>
+  </table>
+  <div id="edit" >
+    <table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <th width="186" scope="row">&nbsp;</th>
+        <th width="443" scope="row">&nbsp;</th>
+        <th width="271" scope="row">&nbsp;</th>
+      </tr>
+      <tr>
+        <th colspan="3" scope="row"><img src="product/Desing_logo.png" width="401" height="63" /></th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <th bgcolor="#CCCCCC" scope="row"><table width="99%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <th colspan="4" scope="row">&nbsp;</th>
+            </tr>
+          <tr>
+            <th colspan="4" scope="row">&nbsp;</th>
+          </tr>
+          <tr>
+            <th colspan="4" scope="row">&nbsp;&nbsp;&nbsp;&nbsp;<span id="add">*เพิ่มข้อมูลสินค้า</span></th>
+          </tr>
+          <tr>
+            <th colspan="4" scope="row">&nbsp;</th>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td width="4%">&nbsp;</td>
+            <td width="63%"><img src="product/beta.png" width="180" height="180" /></td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td><form id="form1" name="form1" method="post" action="">
+              <label>
+                <input type="file" name="pic" id="pic" value="Submit" />
+              </label>
+            </form></td>
+          </tr>
+          <tr >
+            <th colspan="2" style="font-family: 'ms Sans Serif'; font-size: medium;" scope="row"  >&nbsp;</th>
+            <td rowspan="4">&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr >
+            <th width="10%" style="font-family: 'ms Sans Serif'; font-size: medium; text-align: left;" scope="row"  ><span style="text-align: center"></span></th>
+            <th width="23%" style="font-family: 'ms Sans Serif'; font-size: medium; text-align: left;" scope="row"  >หมวดสินค้า</th>
+            <td><form id="form5" name="form5" method="post" action="">
+              <label>
+                <select name="list" id="list" style="width:150 ">
+                  <option>เก้าอี้</option>
+                  <option>โต๊ะ</option>
+                  <option>เตียงนอน</option>
+                  <option>โซฟา</option>
+                  <option>ตู้เสื้อผ้า</option>
+                  <option>ตู้โชว์</option>
+                  <option>ชั้นวางของ</option>
+                </select>
+              </label>
+            </form></td>
+          </tr>
+          <tr >
+            <th colspan="2" style="font-family: 'ms Sans Serif'; font-size: medium;" scope="row"  >&nbsp;</th>
+            <td>&nbsp;</td>
+          </tr>
+          <tr >
+            <th style="font-family: 'ms Sans Serif'; font-size: medium; text-align: left;" scope="row"  >&nbsp;</th>
+            <th style="font-family: 'ms Sans Serif'; font-size: medium; text-align: left;" scope="row"  >ชื่อสินค้า<span style="text-align: left"></span></th>
+            <td><form id="form2" name="form2" method="post" action="">
+              <label>
+                <input type="text" name="name_pro" id="name_pro" />
+                </label>
+            </form></td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <th scope="row">&nbsp;</th>
+            <th scope="row">รายละเอียด</th>
+            <td>&nbsp;</td>
+            <td><form id="form3" name="form3" method="post" action="">
+              <label>
+                <input type="text" name="Detail" id="Detail" />
+              </label>
+            </form></td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <th scope="row">&nbsp;</th>
+            <th scope="row">ราคา</th>
+            <td>&nbsp;</td>
+            <td><form id="form4" name="form4" method="post" action="">
+              <label>
+                <input type="text" name="princ" id="princ" />
+              </label>
+            </form></td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td><form id="form6" name="form6" method="post" action="">
+              <label>
+                <input type="submit" name="save" id="save" value="บันทึก"  style=" width:150px; height:50px;"//>
+              </label>
+            </form></td>
+          </tr>
+          <tr>
+            <th colspan="2" scope="row">&nbsp;</th>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table></th>
+        <th scope="row">&nbsp;</th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+        <th scope="row">&nbsp;</th>
+      </tr>
+    </table>
+  </div>
+</div>
+</body>
+</html>
