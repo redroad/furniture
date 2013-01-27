0cd37db0c198295dab4be66c930155c1bb4cf0e7
etimzaaa <dogtemples@gmail.com>
2013-01-27 22:46:38 +0700

    ddd Signed-off-by: etimzaaa <dogtemples@gmail.com>

---
+++ report_member.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:: Furniture Desing ::</title>
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+}
+#topmenu {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #FFF;
+}
+#topmenu {
+	color: #FFF;
+	font-weight: bold;
+}
+#fb-root2 table {
+	color: #000;
+	text-align: center;
+	font-family: "ms Sans Serif";
+}
+#list {
+	color: #000;
+}
+#fb-root2 #list table tr #list {
+	text-align: center;
+}
+#foot {	text-align: center;
+}
+#foot {
+	font-size: 10px;
+}
+#fb-root2 #list #foot table tr th .Copyright #foot {
+	color: #666;
+}
+#fb-root2 #list table tr #list table {
+	font-size: 12px;
+}
+#MenuList {
+	font-weight: bold;
+}
+#bill {
+	font-weight: bold;
+	color: #03F;
+}
+-->
+</style></head>
+
+<body>
+<div id="fb-root2" >
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <th align="right" valign="middle" bgcolor="#333333" scope="row">::&nbsp;&nbsp;</th>
+      <th align="left" valign="middle" bgcolor="#333333" id="topmenu" scope="row">หน้าแรก&nbsp;&nbsp;&nbsp;    วิธีสั่งซื้อ     &nbsp;&nbsp;การชำระเงิน&nbsp;&nbsp;     การจัดส่งสินค้า&nbsp;&nbsp;     เว็บบอร์ด     &nbsp;&nbsp;ติดต่อเรา  &nbsp;&nbsp;::</th>
+      <th align="right" valign="middle" bgcolor="#333333" scope="row"><table width="186" border="0" cellpadding="0" cellspacing="0">
+        <tr id="topmenu">
+          <th align="left" valign="top" bgcolor="#333333" scope="row" height="31px"><img src="product/login.gif" alt="" width="23" height="31" align="absmiddle" /></th>
+          <th align="left" valign="middle" bgcolor="#333333" scope="row">เข้าสู่ระบบ</th>
+          <th align="left" valign="top" bgcolor="#333333" scope="row"><img src="product/member.gif" alt="" width="22" height="31" align="absmiddle" /></th>
+          <th align="left" valign="middle" bgcolor="#333333" scope="row">สมัครสมาชิก</th>
+        </tr>
+      </table></th>
+    </tr>
+  </table>
+  <div id="list">
+    <table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <th width="132" scope="row">&nbsp;</th>
+        <td width="306">&nbsp;</td>
+        <td width="158">&nbsp;</td>
+        <td width="320">&nbsp;</td>
+        <td width="84">&nbsp;</td>
+      </tr>
+      <tr>
+        <th colspan="5" scope="row"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <th scope="row"><img src="product/Desing_logo.png" width="401" height="63" align="left" /></th>
+            </tr>
+        </table></th>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td colspan="3" id="bill">สมาชิก</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td colspan="3" rowspan="3" id="list"><br />
+
+	<table width="100%" border="1" cellspacing="0" cellpadding="0" bordercolor="#CCCCCC">
+          <tr id="MenuList">
+            <td width="29%" align="center" valign="middle" bgcolor="#CCCCCC" id="list">ชื่อลูกค้า</td>
+            <td width="31%" align="center" valign="middle" bgcolor="#CCCCCC">e-mail</td>
+            <td width="40%" align="center" valign="middle" bgcolor="#CCCCCC">เบอร์โทร</td>
+            </tr>
+          <tr>
+            <td align="left" valign="middle">แมว</td>
+            <td align="left" valign="middle">aa@aa.com</td>
+            <td align="left" valign="middle">0821574784</td>
+            </tr>
+          <tr>
+            <td align="left" valign="middle">ช้าง</td>
+            <td align="left" valign="middle">bb@aa.com</td>
+            <td align="left" valign="middle">0821574899</td>
+            </tr>
+        </table></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td align="left" valign="top">&nbsp;</td>
+      </tr>
+      <tr>
+        <th scope="row">&nbsp;</th>
+        <td align="left" valign="top">&nbsp;</td>
+      </tr>
+    </table>
+    <div id="foot">
+      <table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <th scope="row"></th>
+        </tr>
+        <tr>
+          <th scope="row">&nbsp;</th>
+        </tr>
+        <tr>
+          <th scope="row"><form id="form3" name="form3" method="post" action="index.php">
+            <label>
+              <input type="submit" name="back" id="back" value="กลับหน้าหลัก" />
+            </label>
+          </form></th>
+        </tr>
+        <tr>
+          <th scope="row">&nbsp;</th>
+        </tr>
+        <tr>
+          <th scope="row">&nbsp;</th>
+        </tr>
+        <tr>
+          <th scope="row"><li class="Copyright"><span id="foot">Copyright 2013. Furniture Design. All Rights Reserved. </span></li>&nbsp;</th>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+</body>
+</html>
